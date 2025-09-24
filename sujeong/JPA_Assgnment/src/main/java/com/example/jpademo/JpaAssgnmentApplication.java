608797629b56cfdbdcd37437c415a9a9a7b6ae67
Kim, Sujeong <qpaalaai@gmail.com>
2025-09-24 17:40:05 +0900

    10주차 과제

---
+++ sujeong/JPA_Assgnment/src/main/java/com/example/jpademo/JpaAssgnmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAssgnmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAssgnmentApplication.class, args);
+    }
+
+}
